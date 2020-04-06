8dbf0700dab8a245240cbcbbae962a7be8753f2c
Huahan98 <51449135+Huahan98@users.noreply.github.com>
2020-04-06 02:10:33 -0400

    Add files via upload
    function versioning

--- tf/lambda.tf
+++ tf/lambda.tf
@@ -2,19 +2,21 @@
    region = "us-east-1"
  }
 
- resource "aws_lambda_function" "example" {
+resource "aws_lambda_function" "hello" {
    function_name = "pythonHelloWorld"
    s3_bucket = "python-function"
-   s3_key    = "v1.0.0/pythonFunction.zip"
+   s3_key    = "${var.greeting == "1" ? "v1/Hello" : "v2/Holiday"}.zip"
 
-   handler = "HelloWorld.lambda_handler"
+   handler = "${var.greeting == "1" ? "HelloWorld" : "Holiday"}.lambda_handler"
    runtime = "python3.6"
 
-   role = aws_iam_role.lambda_exec.arn
+   role = aws_iam_role.lambda_exec_hello.arn
  }
 
-resource "aws_iam_role" "lambda_exec"{
-  name = "serverless-example-lambda"
+
+
+ resource "aws_iam_role" "lambda_exec_hello"{
+  name = "serverless-hello-response"
   assume_role_policy = <<EOF
 {
    "Version": "2012-10-17",
@@ -30,4 +32,16 @@ resource "aws_iam_role" "lambda_exec"{
    ]
 }
   EOF
+}
+
+
+resource "aws_lambda_permission" "apigw" {
+   statement_id  = "AllowAPIGatewayInvoke"
+   action        = "lambda:InvokeFunction"
+   function_name = aws_lambda_function.hello.function_name
+   principal     = "apigateway.amazonaws.com"
+
+   # The "/*/*" portion grants access from any method on any resource
+   # within the API Gateway REST API.
+   source_arn = "${aws_api_gateway_rest_api.lambdaGateway.execution_arn}/*/*"
 }
